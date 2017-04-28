7050d7eb47ff7139671313a602bee9d7bd898949
stephen eshelman <objectthink@comcast.net>
2017-04-27 23:44:24 -0400

    keep test code and revise request to use "deviceToken" instead of "token" as the request was being ignored with "token" ( feathersjs reserved keyword? )

--- mim demo/AppDelegate.swift
+++ mim demo/AppDelegate.swift
@@ -11,132 +11,179 @@ import UserNotifications
 import Alamofire
 
 extension String: ParameterEncoding {
-    
-    public func encode(
-        _ urlRequest: URLRequestConvertible,
-        with parameters: Parameters?)throws -> URLRequest
-    {
-        var request = try urlRequest.asURLRequest()
-        request.httpBody = data(using: .utf8, allowLossyConversion: false)
-        return request
-    }
-    
+   
+   public func encode(
+      _ urlRequest: URLRequestConvertible,
+      with parameters: Parameters?)throws -> URLRequest
+   {
+      var request = try urlRequest.asURLRequest()
+      request.httpBody = data(using: .utf8, allowLossyConversion: false)
+      return request
+   }
 }
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
-    
-    var window: UIWindow?
-    
-    // Push notification received
-    func application(
-        _ application: UIApplication,
-        didReceiveRemoteNotification data: [AnyHashable : Any])
-    {
-        // Print notification payload data
-        print("Push notification received: \(data)")
-    }
-    
-    // Called when APNs has assigned the device a unique token
-    func application(
-        _ application: UIApplication,
-        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
-    {
-        // Convert token to string
-        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
-        
-        // Print it to console
-        print("APNs device token: \(deviceTokenString)")
-        
-        // Persist it in your backend in case it's new
-        
-        //send device token to our feathers service
-        Alamofire.request(
-            "http://10.52.50.83:3030/deviceTokens",
-            method: .post,
-            parameters: [:],
-            encoding: "name=\(deviceTokenString)",
-            headers: [:]).responseJSON
-            {
-                (response) in
-                print(response)
-        }
+   
+   var window: UIWindow?
+   
+   // Push notification received
+   func application(
+      _ application: UIApplication,
+      didReceiveRemoteNotification data: [AnyHashable : Any])
+   {
+      // Print notification payload data
+      print("Push notification received: \(data)")
+   }
+   
+   // Called when APNs has assigned the device a unique token
+   func application(
+      _ application: UIApplication,
+      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
+   {
+      // Convert token to string
+      let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+      
+      // Print it to console
+      print("APNs device token: \(deviceTokenString)")
+      
+      // Persist it in your backend in case it's new
+      
+      //send device token to our feathers service
+//      Alamofire.request(
+//         "http://10.52.50.83:3030/deviceTokens",
+//         method: .post,
+//         parameters: [:],
+//         encoding: "deviceid=\(deviceTokenString)",
+//         headers: [:]).responseJSON
+//         {
+//            (response) in
+//            print(response)
+//         }
+      
+      _ = Alamofire.request(
+         "http://10.52.50.83:3030/deviceTokens",
+         method: .post,
+         parameters: ["deviceToken":deviceTokenString],
+         encoding: JSONEncoding.default).responseJSON
+         {
+            (response) in
+            print(response)
+         }
 
-    }
-    
-    // Called when APNs failed to register the device for push notifications
-    func application(
-        _ application: UIApplication,
-        didFailToRegisterForRemoteNotificationsWithError error: Error)
-    {
-        // Print the error to console (you should alert the user that registration failed)
-        print("APNs registration failed: \(error)")
-    }
-    
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
-    {
-        // Override point for customization after application launch.
-        
-        // iOS 10 support
-        if #available(iOS 10, *)
-        {
-            UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound])
-            {
-                (granted, error) in
-            }
-            
-            application.registerForRemoteNotifications()
-        }
-            // iOS 9 support
-        else if #available(iOS 9, *)
-        {
-            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
-            UIApplication.shared.registerForRemoteNotifications()
-        }
-            // iOS 8 support
-        else if #available(iOS 8, *)
-        {
-            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
-            UIApplication.shared.registerForRemoteNotifications()
-        }
-            // iOS 7 support
-        else
-        {
-            application.registerForRemoteNotifications(matching: [.badge, .sound, .alert])
-        }
-        
-        return true
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication)
-    {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication)
-    {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication)
-    {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication)
-    {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication)
-    {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+      
+//      let dict = ["deviceid":deviceTokenString] as [String: Any]
+//      if let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted)
+//      {
+//         let url = NSURL(string: "http://10.52.50.83:3030/deviceTokens")!
+//         let request = NSMutableURLRequest(url: url as URL)
+//         request.httpMethod = "POST"
+//         
+//         request.httpBody = jsonData
+//         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//         
+//         let task = URLSession.shared.dataTask(with: request as URLRequest)
+//         {
+//            data,response,error in
+//            if error != nil
+//            {
+//               print("ERROR")
+//               return
+//            }
+//            
+//            do
+//            {
+//               let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+//               
+//               if let parseJSON = json
+//               {
+//                  //let resultValue:String = parseJSON["success"] as! String;
+//                  //print("result: \(resultValue)")
+//                  print(parseJSON)
+//               }
+//            }
+//            catch let error as NSError
+//            {
+//               print(error)
+//            }        
+//         }          
+//         task.resume()
+//      }
+      
+   }
+   
+   // Called when APNs failed to register the device for push notifications
+   func application(
+      _ application: UIApplication,
+      didFailToRegisterForRemoteNotificationsWithError error: Error)
+   {
+      // Print the error to console (you should alert the user that registration failed)
+      print("APNs registration failed: \(error)")
+   }
+   
+   func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+   {
+      // Override point for customization after application launch.
+
+      // iOS 10 support
+      if #available(iOS 10, *)
+      {
+         UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound])
+         {
+            (granted, error) in
+         }
+         
+         application.registerForRemoteNotifications()
+      }
+         // iOS 9 support
+      else if #available(iOS 9, *)
+      {
+         UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+         UIApplication.shared.registerForRemoteNotifications()
+      }
+         // iOS 8 support
+      else if #available(iOS 8, *)
+      {
+         UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+         UIApplication.shared.registerForRemoteNotifications()
+      }
+         // iOS 7 support
+      else
+      {
+         application.registerForRemoteNotifications(matching: [.badge, .sound, .alert])
+      }
+      
+      return true
+   }
+   
+   func applicationWillResignActive(_ application: UIApplication)
+   {
+      // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+      // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+   }
+   
+   func applicationDidEnterBackground(_ application: UIApplication)
+   {
+      // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+      // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+   }
+   
+   func applicationWillEnterForeground(_ application: UIApplication)
+   {
+      // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+   }
+   
+   func applicationDidBecomeActive(_ application: UIApplication)
+   {
+      // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   }
+   
+   func applicationWillTerminate(_ application: UIApplication)
+   {
+      // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+   }
 }
 
